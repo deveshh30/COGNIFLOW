1f588c0e349d3b507727cd5ad43b91d58c95fc20
deveshh30 <devesh30gupta@gmail.com>
2026-01-29 23:35:50 +0530

    dashboard header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import './App.css'
+import DashHeader from './features/dashboard/ui/DashHeader'
+import Dashboard from './features/dashboard/ui/dashboard'
 
 function App() {
   
 
   return (
     <>
-    
+    <div className="bg-[#1F2937]">
+      <Dashboard/>
+    </div>
+      
     </>
   )
 }
