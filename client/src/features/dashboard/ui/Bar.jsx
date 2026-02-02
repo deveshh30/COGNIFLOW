cbd04e44fb6d2397e955fa35cdde9cdd9bd1a7a7
deveshh30 <devesh30gupta@gmail.com>
2026-02-03 01:02:45 +0530

    timeout delay color in bar

--- client/src/features/dashboard/ui/Bar.jsx
+++ client/src/features/dashboard/ui/Bar.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ProgressBar = ({value}) => {
+
+    const getColor = (value) => {
+    if (value < 40) return "bg-red-500";
+    if (value < 70) return "bg-yellow-400";
+    return "bg-emerald-500";
+  };
+
+
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setWidth(value);
+    }, 300); //100 is to add delay
+
+    return () => clearTimeout(timeout);
+  }, [value]);
   return (
     <div className="mt-4">
 
       <div className="w-full h-2 rounded-full bg-[#2a2a2a] overflow-hidden">
 
         <div
-          className="h-full rounded-full bg-gradient-to-r from-blue-500 to-blue-400 transition-all duration-700 ease-out"
-          style={{ width: `${value}%` }}
+          className={`h-full rounded-full transition-all duration-700 ease-out ${getColor(value)}`}
+          style={{ width: `${width}%` }}
         />
       </div>
 
